252dc6bb2e62e09e4d1abf66b7a3a44d10fe65af
Kevin-nn <thekevincaldon@gmail.com>
2024-10-30 21:39:42 -0500

    env port

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,20 @@ async function bootstrap() {
   // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('lopez.com')
-    .setDescription('api shool ')
+    .setDescription('api school')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('xxx', app, document);
 
-  // Inicia la aplicación en el puerto definido en las variables de entorno o en el puerto 3000 por defecto
-  await app.listen(process.env.PORT || 3000);
+  // Valida que PORT esté definido en las variables de entorno
+  const port = process.env.PORT;
+  if (!port) {
+    throw new Error('La variable de entorno PORT no está definida');
+  }
+
+  // Inicia la aplicación en el puerto definido en las variables de entorno
+  await app.listen(port);
 }
+
 bootstrap();
