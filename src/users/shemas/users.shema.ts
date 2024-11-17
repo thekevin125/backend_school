5261a7e4151b39234665a0ef11fcc4bfc468573e
Kevin-nn <thekevincaldon@gmail.com>
2024-11-17 13:15:03 -0500

    documentacion swagger

--- src/users/shemas/users.shema.ts
+++ src/users/shemas/users.shema.ts
@@ -1,6 +1,8 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { Role } from '../../enums/roles.enum'; // Ajusta la ruta si es necesario
+import { ApiTags, } from '@nestjs/swagger';
+
 
 export type UserDocument = User & Document;
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,49 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto'; // Asegúrate de que la ruta sea correcta
 import { User } from './shemas/users.shema';
 
 @Controller('users')
+@ApiTags('Users') // Agrupa los endpoints bajo la etiqueta "Users" en Swagger
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: User): Promise<User> {
+  @ApiOperation({ summary: 'Crear un nuevo usuario' })
+  @ApiResponse({ status: 201, description: 'Usuario creado exitosamente.', type: User })
+  @ApiResponse({ status: 400, description: 'Datos inválidos.' })
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos los usuarios' })
+  @ApiResponse({ status: 200, description: 'Lista de usuarios.', type: [User] })
   async findAll(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener un usuario por ID' })
+  @ApiResponse({ status: 200, description: 'Detalles del usuario.', type: User })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
   async findOne(@Param('id') id: string): Promise<User> {
     return this.usersService.findOneById(id);
   }
 
   @Put(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: User): Promise<User> {
+  @ApiOperation({ summary: 'Actualizar un usuario' })
+  @ApiResponse({ status: 200, description: 'Usuario actualizado.', type: User })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
+  async update(@Param('id') id: string, @Body() updateUserDto: CreateUserDto): Promise<User> {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un usuario' })
+  @ApiResponse({ status: 200, description: 'Usuario eliminado.' })
+  @ApiResponse({ status: 404, description: 'Usuario no encontrado.' })
   async remove(@Param('id') id: string): Promise<void> {
     return this.usersService.remove(id);
   }
