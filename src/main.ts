40e1fdffb884ad2589b013fc97bb099bcdfe93a5
Kevin-nn <thekevincaldon@gmail.com>
2024-11-20 12:22:59 -0500

    app.enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,23 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Permitir solicitudes desde el frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
+    credentials: true, // Permitir envío de cookies o encabezados de autenticación
+  });
+
   // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('lopez.com')
-    .setDescription('api school')
+    .setDescription('API School')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('xxx', app, document);
 
-  // Define el puerto desde la variable de entorno PORT de Render o usa 4000 como predeterminado
+  // Define el puerto desde la variable de entorno PORT o usa 4000 como predeterminado
   const port = process.env.PORT || 4000;
 
   // Inicia la aplicación en el puerto definido
--- src/users/shemas/users.shema.ts
+++ src/users/shemas/users.shema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { Role } from '../../enums/roles.enum'; // Ajusta la ruta si es necesario
-import { ApiTags, } from '@nestjs/swagger';
 
 
 export type UserDocument = User & Document;
